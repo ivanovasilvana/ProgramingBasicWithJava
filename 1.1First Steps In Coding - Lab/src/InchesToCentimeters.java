b0647faffb29e212f68c13319ef21240f965db50
ivanovasilvana <71032525+ivanovasilvana@users.noreply.github.com>
2021-02-20 14:22:21 +0200

    update

--- 1.1First Steps In Coding - Lab/src/InchesToCentimeters.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class InchesToCentimeters {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        double inches = scanner.nextDouble();
-        double santimetres = inches * 2.54;
-        System.out.println(santimetres);
-    }
-}
--- 1.1First Steps In Coding - Lab/src/SquareArea.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class SquareArea {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int area = a * a;
-        System.out.println(area);
-    }
-}
